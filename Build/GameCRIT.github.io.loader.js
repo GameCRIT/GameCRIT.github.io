a7becd7c5a2e85703bfd8a4e4663b7429a78ec68
PetersOfTheNight <pedroneves.art@gmail.com>
2025-06-09 19:11:30 +0100

    fullstacktrace

--- Build/GameCRIT.github.io.loader.js
+++ Build/GameCRIT.github.io.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 94802627,
+    wasmFileSize: 172767749,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
